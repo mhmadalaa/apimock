029cedfd356347b427d0d5174211be6b51914014
Mohamed Alaa <mhmadalaa@mans.edu.eg>
2024-09-26 19:01:19 +0300

    add img

--- api.js
+++ api.js
@@ -41,42 +41,48 @@ router.get('/fertilizers', (req, res) => {
 router.get('/plants', (req, res) => {
   const plants = [
     {
-      "name":"Mint",
-      "days-to-harvest": "60 - 90 days",
-      "watering freq": " Water regularly; keep the soil consistently moist, about 2-3 times per week.",
-      "watering amount": "16 - 33 ml of water weekly",
-      "fertilizer": "Organic compost or balanced fertilizer (10-10-10) once a month.",
-      "fertilization cycle": " Once per growing season.",
-      "soil": "Well-drained, loamy soil rich in organic matter.",
-      "temperature": "15 - 21°C",
-      "steps to grow": [
-        "Choose a location: Mint prefers partial shade but can tolerate full sun if watered frequently.",
-        "Prepare the soil: Use well-drained, loamy soil rich in organic matter.",
-        "Planting: You can plant mint either from seeds, cuttings, or transplants. Space plants about 18-24 inches apart.",
-        "Watering: Water regularly, keeping the soil consistently moist. Ensure the plant receives 1-2 inches (16-33 ml) of water per week.",
-        "Fertilize: Use organic compost or balanced fertilizer (10-10-10) once a month.",
-        "Harvest: You can start harvesting mint leaves after 60-90 days by pinching off leaves or stems as needed."
-      ]
-    }, 
+      name: 'Mint',
+      'days-to-harvest': '60 - 90 days',
+      'watering freq':
+        ' Water regularly; keep the soil consistently moist, about 2-3 times per week.',
+      'watering amount': '16 - 33 ml of water weekly',
+      fertilizer:
+        'Organic compost or balanced fertilizer (10-10-10) once a month.',
+      'fertilization cycle': ' Once per growing season.',
+      soil: 'Well-drained, loamy soil rich in organic matter.',
+      temperature: '15 - 21°C',
+      'steps to grow': [
+        'Choose a location: Mint prefers partial shade but can tolerate full sun if watered frequently.',
+        'Prepare the soil: Use well-drained, loamy soil rich in organic matter.',
+        'Planting: You can plant mint either from seeds, cuttings, or transplants. Space plants about 18-24 inches apart.',
+        'Watering: Water regularly, keeping the soil consistently moist. Ensure the plant receives 1-2 inches (16-33 ml) of water per week.',
+        'Fertilize: Use organic compost or balanced fertilizer (10-10-10) once a month.',
+        'Harvest: You can start harvesting mint leaves after 60-90 days by pinching off leaves or stems as needed.',
+      ],
+      image: 'https://apimock-delta.vercel.app/img/2.png',
+    },
     {
-      "name":"Mint",
-      "days-to-harvest": "60 - 90 days",
-      "watering freq": " Water regularly; keep the soil consistently moist, about 2-3 times per week.",
-      "watering amount": "16 - 33 ml of water weekly",
-      "fertilizer": "Organic compost or balanced fertilizer (10-10-10) once a month.",
-      "fertilization cycle": " Once per growing season.",
-      "soil": "Well-drained, loamy soil rich in organic matter.",
-      "temperature": "15 - 21°C",
-      "steps to grow": [
-        "Choose a location: Mint prefers partial shade but can tolerate full sun if watered frequently.",
-        "Prepare the soil: Use well-drained, loamy soil rich in organic matter.",
-        "Planting: You can plant mint either from seeds, cuttings, or transplants. Space plants about 18-24 inches apart.",
-        "Watering: Water regularly, keeping the soil consistently moist. Ensure the plant receives 1-2 inches (16-33 ml) of water per week.",
-        "Fertilize: Use organic compost or balanced fertilizer (10-10-10) once a month.",
-        "Harvest: You can start harvesting mint leaves after 60-90 days by pinching off leaves or stems as needed."
-      ]
-    }, 
-  ]
+      name: 'Mint',
+      'days-to-harvest': '60 - 90 days',
+      'watering freq':
+        ' Water regularly; keep the soil consistently moist, about 2-3 times per week.',
+      'watering amount': '16 - 33 ml of water weekly',
+      fertilizer:
+        'Organic compost or balanced fertilizer (10-10-10) once a month.',
+      'fertilization cycle': ' Once per growing season.',
+      soil: 'Well-drained, loamy soil rich in organic matter.',
+      temperature: '15 - 21°C',
+      'steps to grow': [
+        'Choose a location: Mint prefers partial shade but can tolerate full sun if watered frequently.',
+        'Prepare the soil: Use well-drained, loamy soil rich in organic matter.',
+        'Planting: You can plant mint either from seeds, cuttings, or transplants. Space plants about 18-24 inches apart.',
+        'Watering: Water regularly, keeping the soil consistently moist. Ensure the plant receives 1-2 inches (16-33 ml) of water per week.',
+        'Fertilize: Use organic compost or balanced fertilizer (10-10-10) once a month.',
+        'Harvest: You can start harvesting mint leaves after 60-90 days by pinching off leaves or stems as needed.',
+      ],
+      image: 'https://apimock-delta.vercel.app/img/2.png',
+    },
+  ];
   res.json(plants);
 });
 
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // server.js
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware to parse JSON requests
 app.use(express.json());
